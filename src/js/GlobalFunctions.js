33e57e7af31b272a21262cfef6dc45cf7a442448
orlandophillips <phillips.ow@oundleschool.org.uk>
2025-11-07 13:46:48 +0000

    Replaced Ding's design

--- src/js/GlobalFunctions.js
+++ src/js/GlobalFunctions.js
@@ -1,20 +1,2 @@
-// Archive containing re-usable custom classes for this project
 
-export class Inventory { // Parent class of Item
-    constructor() {
-
-    }
-}
-
-export class Item extends Inventory { // Subclass derived from Inventory (Inherits methods)
-    constructor() {
-
-    }
-}
-
-export class Test {
-    constructor() {
-        
-    }
-}
 
